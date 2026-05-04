9e378f20ebdd388ee52350ea9c55f56575c56af0
chukmunnlee <chukmunnlee@gmail.com>
2026-05-04 14:16:07 +0800

    Added shell and exec form

--- workshop01/task01/dov-bear/golang/create-docker-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl create secret docker-registry dov-bear-registry -nworkshop03 \
-  --docker-server=registry.gitlab.com \
-  --docker-username=__USER__ \
-  --docker-password=__PASSWORD__ \
-  --docker-email=__EMAIL__ \
-  --dry-run=client -oyaml
-
